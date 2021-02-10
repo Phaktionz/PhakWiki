dc61fd7dffdd2581138d113d330722f1be667413
mustafif0929@gmail.com <mustafif0929@gmail.com>
2021-02-09 19:10:09 -0500

    Fixed menu

--- src/index.js
+++ src/index.js
@@ -1,6 +1,5 @@
 const { app, BrowserWindow } = require('electron');
 const path = require('path');
-
 // Handle creating/removing shortcuts on Windows when installing/uninstalling.
 if (require('electron-squirrel-startup')) { // eslint-disable-line global-require
   app.quit();
@@ -17,22 +16,117 @@ const createWindow = () => {
   mainWindow.loadFile(path.join(__dirname, 'index.html'));
  
   //Where to have custom menu
-    const { Menu, MenuItem } = require('electron')
 
-    const menu = new Menu()
+const { Menu, MenuItem } = require('electron');
+
+const menu = new Menu()
     menu.append(new MenuItem({
-        label: '|Phaktionz|',
+        label: 'Phaktionz',
         submenu: [
         {
-        label: 'Rules',
-        click: () => { win.loadFile('src/lib/')}
+            label: 'Home', 
+            accelerator: 'Ctrl+P',
+            click: () => {mainWindow.loadFile('src/index.html')}
+        },
+        {
+            label: 'Rules',
+            click: () => { mainWindow.loadFile('src/lib/Plan.pdf')}
+        }, 
+        {
+            label: 'Quit', 
+            role: 'quit', 
+            accelerator: 'Ctrl+Q', 
+            click: function() { app.quit(); }
         }
         ]
     }))
+    
+    menu.append(new MenuItem({
+     label: 'Edit',
+     submenu: [
+        {
+          label: 'Undo',
+          accelerator: 'CmdOrCtrl+Z',
+          role: 'undo'
+        },
+        {
+          label: 'Redo',
+          accelerator: 'Shift+CmdOrCtrl+Z',
+          role: 'redo'
+        },
+        {
+          type: 'separator'
+        },
+        {
+          label: 'Cut',
+          accelerator: 'CmdOrCtrl+X',
+          role: 'cut'
+        },
+        {
+          label: 'Copy',
+          accelerator: 'CmdOrCtrl+C',
+          role: 'copy'
+        },
+        {
+          label: 'Paste',
+          accelerator: 'CmdOrCtrl+V',
+          role: 'paste'
+        },
+        {
+          label: 'Select All',
+          accelerator: 'CmdOrCtrl+A',
+          role: 'selectall'
+        }
+    ]
+    }))
+    menu.append(new MenuItem({
+      label: 'View',
+      submenu: [
+        {
+          label: 'Reload',
+          accelerator: 'CmdOrCtrl+R',
+          click: function(item, focusedWindow) {
+            if (focusedWindow)
+              focusedWindow.reload();
+          }
+        },
+        {
+          label: 'Toggle Full Screen',
+          accelerator: (function() {
+            if (process.platform == 'darwin')
+              return 'Ctrl+Command+F';
+            else
+              return 'F11';
+          })(),
+          click: function(item, focusedWindow) {
+            if (focusedWindow)
+              focusedWindow.setFullScreen(!focusedWindow.isFullScreen());
+          }
+        }
+        ]
+    }))
+    
+    menu.append(new MenuItem({
+      label: 'Window',
+      role: 'window',
+      submenu: [
+        {
+          label: 'Minimize',
+          accelerator: 'CmdOrCtrl+M',
+          role: 'minimize'
+        },
+        {
+          label: 'Close',
+          accelerator: 'CmdOrCtrl+W',
+          role: 'close'
+        },
+      ]
+    }))
   
-  
-};
+  Menu.setApplicationMenu(menu)
 
+    
+};
 // This method will be called when Electron has finished
 // initialization and is ready to create browser windows.
 // Some APIs can only be used after this event occurs.
